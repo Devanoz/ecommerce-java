ca83b7268797ac65daa6cee45d5bb9d13b7f08fd
Muhamamad Devano Zaidan <80044118+Devanoz@users.noreply.github.com>
2024-12-24 23:36:20 +0700

    first commit

---
+++ src/main/java/com/ecommerce/fast_campus_ecommerce/FastCampusEcommerceApplication.java
@@ -0,0 +1,12 @@
+package com.ecommerce.fast_campus_ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastCampusEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastCampusEcommerceApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ecommerce/fast_campus_ecommerce/FastCampusEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.fast_campus_ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastCampusEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
