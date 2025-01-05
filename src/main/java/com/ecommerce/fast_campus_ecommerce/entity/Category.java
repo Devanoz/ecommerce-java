2eb749aefedbf07d0808b3b128d9824736659096
Muhamamad Devano Zaidan <80044118+Devanoz@users.noreply.github.com>
2025-01-05 10:47:55 +0700

    add category entity

---
+++ src/main/java/com/ecommerce/fast_campus_ecommerce/entity/Category.java
@@ -0,0 +1,33 @@
+package com.ecommerce.fast_campus_ecommerce.entity;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "category")
+@Data
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "category_id")
+    private Long categoryId;
+
+    @Column(name = "name", nullable = false, length = 255)
+    private String name;
+
+    @Column(name = "description", columnDefinition = "TEXT")
+    private String description;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
