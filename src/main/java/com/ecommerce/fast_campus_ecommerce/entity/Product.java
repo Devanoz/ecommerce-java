105134258cad9b295048dd17cdf805e87af11191
Muhamamad Devano Zaidan <80044118+Devanoz@users.noreply.github.com>
2025-01-05 10:46:15 +0700

    add version 1 migration database entity: - product - category - product_category

---
+++ src/main/java/com/ecommerce/fast_campus_ecommerce/entity/Product.java
@@ -0,0 +1,42 @@
+package com.ecommerce.fast_campus_ecommerce.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "product")
+@Data
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "product_id")
+    private Long productId;
+
+    @Column(name = "name", nullable = false, length = 255)
+    private String name;
+
+    @Column(name = "description", columnDefinition = "TEXT")
+    private String description;
+
+    @Column(name = "price", nullable = false)
+    private BigDecimal price;
+
+    @Column(name = "stock_quantity", nullable = false)
+    private int stockQuantity;
+
+    @Column(name = "weight", nullable = false)
+    private BigDecimal weight;
+
+    @Column(name = "created_at", nullable = false,updatable = false)
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
