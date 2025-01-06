f2f0228c3e5996ab5b9962acccb096ceafe240de
Muhamamad Devano Zaidan <80044118+Devanoz@users.noreply.github.com>
2025-01-06 12:43:40 +0700

    add GenericExceptionHandler

---
+++ src/main/java/com/ecommerce/fast_campus_ecommerce/config/middleware/GenericExceptionHandler.java
@@ -0,0 +1,53 @@
+package com.ecommerce.fast_campus_ecommerce.config.middleware;
+
+import com.ecommerce.fast_campus_ecommerce.common.errors.ResourceNotFoundException;
+import com.ecommerce.fast_campus_ecommerce.common.errors.RoleNotFoundException;
+import com.ecommerce.fast_campus_ecommerce.common.errors.UserNotFoundException;
+import com.ecommerce.fast_campus_ecommerce.model.ErrorResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GenericExceptionHandler {
+
+    @ExceptionHandler({
+            ResourceNotFoundException.class,
+            UserNotFoundException.class,
+            RoleNotFoundException.class
+    })
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public @ResponseBody ErrorResponse handleResourceNotFoundException(HttpServletRequest req,
+                                                                       ResourceNotFoundException exception) {
+        return ErrorResponse.builder()
+                .code(HttpStatus.NOT_FOUND.value())
+                .message(exception.getMessage())
+                .timestamp(LocalDateTime.now())
+                .build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public @ResponseBody ErrorResponse handleValidationException(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach(objectError -> {
+            String fieldName = ((FieldError) objectError).getField();
+            String errorMessage = objectError.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return ErrorResponse.builder()
+                .code(HttpStatus.BAD_REQUEST.value())
+                .message(errors.toString())
+                .timestamp(LocalDateTime.now())
+                .build();
+    }
+}
