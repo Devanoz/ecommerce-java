9e5284d8177ced2c0979246e215a89e5fb6e6411
Muhamamad Devano Zaidan <80044118+Devanoz@users.noreply.github.com>
2025-01-06 12:43:17 +0700

    add product controller endpoints

--- src/main/java/com/ecommerce/fast_campus_ecommerce/controllers/ProductController.java
+++ src/main/java/com/ecommerce/fast_campus_ecommerce/controllers/ProductController.java
@@ -1,5 +1,73 @@
 package com.ecommerce.fast_campus_ecommerce.controllers;
 
-public class ProductController {
+import com.ecommerce.fast_campus_ecommerce.model.PaginatedProductResponse;
+import com.ecommerce.fast_campus_ecommerce.model.ProductRequest;
+import com.ecommerce.fast_campus_ecommerce.model.ProductResponse;
+import com.ecommerce.fast_campus_ecommerce.service.ProductService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("products")
+@RequiredArgsConstructor
+public class ProductController {
+    private final ProductService productService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductResponse> findProductById(@PathVariable Long id) {
+        ProductResponse productResponse = productService.findById(id);
+        return ResponseEntity.ok(productResponse);
+    }
+
+    @GetMapping("")
+    public ResponseEntity<PaginatedProductResponse> findAllProducts(
+            @RequestParam(defaultValue = "10", name = "size") Integer size,
+            @RequestParam(defaultValue = "0",name="page") Integer page,
+            @RequestParam(defaultValue = "product_id,asc" , name = "sort") String sort,
+            @RequestParam(name = "name",required = false) String name
+    ) {
+        Sort sorting;
+        if(sort.contains(",")) {
+            String[] split = sort.split(",");
+            sorting = Sort.by(getSorDirection(split[1]),split[0]);
+        }else{
+            sorting = Sort.by(getSorDirection(sort));
+        }
+        PaginatedProductResponse productResponses;
+        if(name!=null && !name.isEmpty()) {
+            productResponses = productService.findAllByNameAndPageable(name,page,size,sorting);
+        }else{
+            productResponses  = productService.findAllByPage(page, size, sorting);
+        }
+        return ResponseEntity.ok(productResponses);
+    }
+
+    @PostMapping("/products")
+    public ResponseEntity<ProductResponse> createProduct(@RequestBody @Valid ProductRequest productRequest) {
+        ProductResponse productResponse = productService.create(productRequest);
+        return ResponseEntity.ok(productResponse);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ProductResponse> updateProduct(@PathVariable Long id, @RequestBody @Valid ProductRequest productRequest) {
+        ProductResponse productResponse = productService.update(id, productRequest);
+        return ResponseEntity.ok(productResponse);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
+        productService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    private Sort.Direction getSorDirection(String sort){
+        if(sort.equals("asc")) return Sort.Direction.ASC;
+        else return Sort.Direction.DESC;
+    }
 }
