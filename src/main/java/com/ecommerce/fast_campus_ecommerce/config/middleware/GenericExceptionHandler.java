ea57fd50687ea136214616fd8417222d9fc2dd5a
Muhamamad Devano Zaidan <80044118+Devanoz@users.noreply.github.com>
2025-01-07 16:27:51 +0700

    add error for authentication

--- src/main/java/com/ecommerce/fast_campus_ecommerce/config/middleware/GenericExceptionHandler.java
+++ src/main/java/com/ecommerce/fast_campus_ecommerce/config/middleware/GenericExceptionHandler.java
@@ -1,11 +1,16 @@
 package com.ecommerce.fast_campus_ecommerce.config.middleware;
 
-import com.ecommerce.fast_campus_ecommerce.common.errors.ResourceNotFoundException;
-import com.ecommerce.fast_campus_ecommerce.common.errors.RoleNotFoundException;
-import com.ecommerce.fast_campus_ecommerce.common.errors.UserNotFoundException;
+import com.ecommerce.fast_campus_ecommerce.common.errors.*;
 import com.ecommerce.fast_campus_ecommerce.model.ErrorResponse;
+import io.jsonwebtoken.ExpiredJwtException;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AccountStatusException;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.InsufficientAuthenticationException;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -13,11 +18,14 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import java.nio.file.AccessDeniedException;
+import java.security.SignatureException;
 import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
 @ControllerAdvice
+@Slf4j
 public class GenericExceptionHandler {
 
     @ExceptionHandler({
@@ -35,6 +43,48 @@ public class GenericExceptionHandler {
                 .build();
     }
 
+    @ExceptionHandler(BadRequestException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public @ResponseBody ErrorResponse handleBadRequestException(HttpServletRequest req,
+                                                                 BadRequestException exception) {
+        return ErrorResponse.builder()
+                .code(HttpStatus.BAD_REQUEST.value())
+                .message(exception.getMessage())
+                .timestamp(LocalDateTime.now())
+                .build();
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public @ResponseBody ErrorResponse handleGenericException(HttpServletRequest req,
+                                                              HttpServletResponse resp,
+                                                              Exception exception) {
+        log.error("Terjadi error. status code: " + HttpStatus.INTERNAL_SERVER_ERROR + "error message: "
+                + exception.getMessage());
+        if (exception instanceof BadCredentialsException ||
+                exception instanceof AccountStatusException ||
+                exception instanceof AccessDeniedException ||
+                exception instanceof SignatureException ||
+                exception instanceof ExpiredJwtException ||
+                exception instanceof AuthenticationException ||
+                exception instanceof InsufficientAuthenticationException
+        ) {
+
+            resp.setStatus(HttpServletResponse.SC_FORBIDDEN);
+            return ErrorResponse.builder()
+                    .code(HttpStatus.FORBIDDEN.value())
+                    .message(exception.getMessage())
+                    .timestamp(LocalDateTime.now())
+                    .build();
+        }
+        resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        return ErrorResponse.builder()
+                .code(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                .message(exception.getMessage())
+                .timestamp(LocalDateTime.now())
+                .build();
+    }
+
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public @ResponseBody ErrorResponse handleValidationException(MethodArgumentNotValidException ex) {
@@ -47,6 +97,31 @@ public class GenericExceptionHandler {
         return ErrorResponse.builder()
                 .code(HttpStatus.BAD_REQUEST.value())
                 .message(errors.toString())
+                .timestamp(LocalDateTime.now())
+                .build();
+    }
+
+    @ExceptionHandler({
+            UsernameAlreadyExistsException.class,
+            EmailAlreadyExistsException.class
+    })
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public @ResponseBody ErrorResponse handleConflictException(HttpServletRequest req,
+                                                               RuntimeException exception) {
+        return ErrorResponse.builder()
+                .code(HttpStatus.CONFLICT.value())
+                .message(exception.getMessage())
+                .timestamp(LocalDateTime.now())
+                .build();
+    }
+
+    @ExceptionHandler(InvalidPasswordException.class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public @ResponseBody ErrorResponse handleInvalidPasswordException(HttpServletRequest req,
+                                                                      InvalidPasswordException exception) {
+        return ErrorResponse.builder()
+                .code(HttpStatus.UNAUTHORIZED.value())
+                .message(exception.getMessage())
                 .timestamp(LocalDateTime.now())
                 .build();
     }
